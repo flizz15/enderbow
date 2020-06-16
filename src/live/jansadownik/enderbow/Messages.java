0f1d4bbd092876dbe9ec0bdaa1605305ec159c4d
flizz <kakun12311@gmail.com>
2020-06-16 23:12:53 +0200

    added more permissions, message parsing and enderpearl using

--- src/live/jansadownik/enderbow/Messages.java
+++ src/live/jansadownik/enderbow/Messages.java
@@ -1,7 +1,19 @@
 package live.jansadownik.enderbow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Messages {
     public static String CommandNoSuchPlayer = "No such player on the server";
     public static String CommandTypePlayerName = "Type targeted Player name!";
-    public static String PlayerNotHavePermission = "You don't have permission";
+    public static String PlayerNotHavePermission = "You don't have permission!";
+    public static String PlayerNotHaveItem = "You don't have %item%!";
+
+    public static String parseMessage(String message, HashMap<String, String> parameters) {
+        String messageToReturn = message;
+        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+            messageToReturn = messageToReturn.replace(entry.getKey(), entry.getValue());
+        }
+        return messageToReturn;
+    }
 }
--- src/live/jansadownik/enderbow/listeners/ArrowLaunchListener.java
+++ src/live/jansadownik/enderbow/listeners/ArrowLaunchListener.java
@@ -1,12 +1,16 @@
 package live.jansadownik.enderbow.listeners;
 
 import live.jansadownik.enderbow.Messages;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
 
 public class ArrowLaunchListener implements Listener {
 
@@ -14,11 +18,28 @@ public class ArrowLaunchListener implements Listener {
     public void onArrowLaunch(EntityShootBowEvent event) {
         if (event.getEntity() instanceof Player)
             if (event.getBow().getEnchantmentLevel(Enchantment.ARROW_INFINITE) == 2) {
-                if (!event.getEntity().hasPermission("enderbow.use.enderbow")) {
+                Player p = (Player) event.getEntity();
+                if (!p.hasPermission("enderbow.use.enderbow")) {
                     event.setCancelled(true);
-                    event.getEntity().sendMessage(Messages.PlayerNotHavePermission);
+                    p.sendMessage(Messages.PlayerNotHavePermission);
                     return;
                 }
+                if (!p.hasPermission("enderbow.bypass.use.enderpearl")) {
+                    if (!p.getInventory().contains(Material.ENDER_PEARL)) {
+                        event.setCancelled(true);
+                        event.getEntity().sendMessage(
+                                Messages.parseMessage(Messages.PlayerNotHaveItem, new HashMap<String, String>() {{
+                                    put("%item%", "ender pearl");
+                                }})
+                        );
+                        return;
+                    } else {
+//                        p.getInventory().remove(new ItemStack(Material.ENDER_PEARL, 1));
+                        p.getInventory().removeItem(new ItemStack(Material.ENDER_PEARL, 1));
+                        p.updateInventory();
+                    }
+                }
+
                 Entity arrowToLaunch = event.getProjectile();
                 arrowToLaunch.setCustomName("EnderBowArrow");
                 arrowToLaunch.setVelocity(arrowToLaunch.getVelocity().multiply(1.2));
